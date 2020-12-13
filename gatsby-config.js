81e93dfb8420e0af97d084f0012e2e13cc58cc59
Mohammed-elmredi <medelmredi@gmail.com>
2020-12-13 20:16:30 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  plugins: [`gatsby-plugin-netlify-cms`],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `My Blog Title`,
+    author: `My Name`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+}
